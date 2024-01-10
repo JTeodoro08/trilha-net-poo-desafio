af4d4e4756bdbe95a484ddff4253ccb029998072
JTeodoro08 <jteodoro33jc@gmail.com>
2024-01-09 23:28:15 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!!!
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphoneXR = new Iphone("11972395229", "xr-1105", "555-333-444-222", 128);
+iphoneXR.ReceberLigacao();
+iphoneXR.InstalarAplicativo("VerboFM");
+iphoneXR.Ligar();
+
+Console.WriteLine("-----------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone nokia_1000 = new Nokia("11972395229", "nokia-1105", "666-333-222-333", 256);
+nokia_1000.ReceberLigacao();
+nokia_1000.InstalarAplicativo("Dtel Telecom");
+nokia_1000.Ligar();
